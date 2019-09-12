8f466305db4f85a079bebf0e786b8df961578cd9
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-09-12 21:34:31 +0530

    PostSerializer: expose tag_list key for posts

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 # Post Serializer
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :views,:post_id,:slug
+  attributes :id, :title, :body, :views,:post_id,:slug,:tag_list
   has_one :user
   has_one :account
   has_many :comments
