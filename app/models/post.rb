f164234e5ab98c36681a00d16d43ba8454d67147
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-09-12 21:01:37 +0530

    PostModel: create slug on title and expose it via serializers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 # Post model
 class Post < ApplicationRecord
+  extend FriendlyId
   belongs_to :user
   belongs_to :account
   has_many :comments, class_name: 'Post', foreign_key: 'post_id'
   acts_as_taggable
+  friendly_id :title, use: :slugged
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 # Post Serializer
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :views,:post_id
+  attributes :id, :title, :body, :views,:post_id,:slug
   has_one :user
   has_one :account
   has_many :comments
